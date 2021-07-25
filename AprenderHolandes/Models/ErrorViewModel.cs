70ca1f0e40ea31055c3ebeae50666596b03a92f3
Tom Maenhout <tom.maenhout@gmail.com>
2021-07-25 17:20:53 -0300

    Add project files.

---
+++ AprenderHolandes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AprenderHolandes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
