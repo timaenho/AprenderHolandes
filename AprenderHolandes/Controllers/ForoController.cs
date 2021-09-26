7e5d72f6bac0817b752e52dc41a83ea25e7a6334
Tom Maenhout <tom.maenhout@gmail.com>
2021-09-26 20:53:38 -0300

    foro indexAlumno

--- AprenderHolandes/Controllers/ForoController.cs
+++ AprenderHolandes/Controllers/ForoController.cs
@@ -7,26 +7,51 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AprenderHolandes.Data;
 using AprenderHolandes.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AprenderHolandes.Controllers
 {
     public class ForoController : Controller
     {
         private readonly DbContextInstituto _context;
+        private readonly UserManager<Persona> _usermanager;
 
-        public ForoController(DbContextInstituto context)
+        public ForoController(DbContextInstituto context, UserManager<Persona> userManager)
         {
             _context = context;
+            _usermanager = userManager;
         }
 
         // GET: Foro
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> IndexAlumno()
         {
-            var dbContextInstituto = _context.Mensajes.Include(m => m.MateriaCursada).Include(m => m.Persona);
-            return View(await dbContextInstituto.ToListAsync());
+            var Alumno = (Alumno) await _usermanager
+                .GetUserAsync(HttpContext.User);
+            var _Alumno = _context.Alumnos
+                .Include(a => a.AlumnosMateriasCursadas)
+                .ThenInclude(amc => amc.MateriaCursada)
+                .FirstOrDefault(a => a.Id == Alumno.Id);
+
+            var materiaCursada = Alumno.AlumnosMateriasCursadas
+                .FirstOrDefault(amc => amc.MateriaCursada.Activo);
+
+            var materiaCursadaId = materiaCursada.MateriaCursadaId;
+
+            var preguntas = _context.Preguntas
+                .Include(m => m.MateriaCursada)
+                .Include(m => m.Persona)
+                .Where(p => p.MateriaCursadaId == materiaCursadaId);
+
+            return View(await preguntas.ToListAsync());
         }
 
         // GET: Foro/Details/5
+
+        public async Task<IActionResult> IndexProfesor()
+        {
+  
+  
+        }
         public async Task<IActionResult> Details(Guid? id)
         {
             if (id == null)
